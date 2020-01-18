981adfd020f953e1d26fc6e870b59291c9d2bc17
Mirosław Karczmarczyk <fnxnet@gmail.com>
2020-01-18 08:55:42 +0100

    fixing import path in helper tests

--- cqrs/helper_test.go
+++ cqrs/helper_test.go
@@ -1,24 +1,23 @@
 package cqrs
 
 import (
-    "testing"
-
-    "fnxnet/cqrs/cqrs/dummy"
-    "github.com/stretchr/testify/assert"
+	"github.com/fnxnet/go-cqrs/cqrs/dummy"
+	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 type testCommand struct{}
 
 func TestExtractName(t *testing.T) {
-    expected := "cqrs.testCommand"
-    assert.Equal(t, expected, extractName(testCommand{}))
-    assert.Equal(t, expected, extractName(&testCommand{}))
-    assert.Equal(t, expected, extractName(*(&testCommand{})))
+	expected := "cqrs.testCommand"
+	assert.Equal(t, expected, extractName(testCommand{}))
+	assert.Equal(t, expected, extractName(&testCommand{}))
+	assert.Equal(t, expected, extractName(*(&testCommand{})))
 }
 
 func TestExtractNameDifferentPackage(t *testing.T) {
-    expected := "dummy.DummyCommand"
-    assert.Equal(t, expected, extractName(dummy.DummyCommand{}))
-    assert.Equal(t, expected, extractName(&dummy.DummyCommand{}))
-    assert.Equal(t, expected, extractName(*(&dummy.DummyCommand{})))
+	expected := "dummy.DummyCommand"
+	assert.Equal(t, expected, extractName(dummy.DummyCommand{}))
+	assert.Equal(t, expected, extractName(&dummy.DummyCommand{}))
+	assert.Equal(t, expected, extractName(*(&dummy.DummyCommand{})))
 }
